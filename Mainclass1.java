727005e7a37fcfe0ee114fbe7421c4fbd77a15dd
sanjay9035 <52272417+sanjay9035@users.noreply.github.com>
2019-09-26 16:34:43 +0530

    Add files via upload

---
+++ Mainclass1.java
@@ -0,0 +1,15 @@
+class Mainclass
+{
+
+   public static void main(String[] args)
+   {
+      
+        System.out.println("*");
+        System.out.println("*");
+        System.out.println("*");
+
+
+   }
+
+            
+}
